27ed056c0e401ceea40f96fb0fa8201ded8ddc29
130349 <wanghui@che001.com>
2017-05-24 13:23:29 +0800

    add files

---
+++ README.md
@@ -0,0 +1,11 @@
+# hotfix-auto-output
+
+### Install 
+`npm install`
+
+### Usage
+`gulp`
+
+### Config
++ `codePath` 代码目录
++ `outFolder` 输入目录
---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+    git = require('gulp-git'),
+    merge = require('merge-stream'),
+    codePath = 'd:\\gitCode\\qyd\\deploy\\p2p\\src\\main\\webapp\\',// 【\】需要转译
+    outFolder = 'e:\\build',
+    outputPath = [],
+    files = [];
+
+// Run git init
+// src is the root folder for git to initialize
+gulp.task('init', function () {
+    git.init(function (err) {
+        if (err) throw err;
+    });
+});
+
+// Working tree status
+gulp.task('status', function (cb) {
+    return git.status({ cwd: codePath }, function (err, stdout) {
+        if (err) throw err;
+        files = stdout.match(/modified:(.)*/g);
+        files = files.map(function (item) {
+            return codePath + item.replace("modified:", '').replace(/\//g, "\\").trim();
+        });
+        cb(err);
+    });
+});
+
+// copy
+gulp.task("copy", ["status"], function () {
+    outputPath = [
+        outFolder,
+        new Date().toLocaleDateString(),
+        "ROOT"
+    ];
+    var tasks = files.map(function (file) {
+        var output = outputPath.join('\\');
+        output += '\\' + file.replace(codePath, '').split("\\").slice(0, file.replace(codePath, '').split("\\").length - 1).join("\\");
+        return gulp
+            .src(file)
+            .pipe(gulp.dest(output));
+    });
+    return merge(tasks);
+});
+
+// default
+gulp.task("default", ["init", "status", "copy"]);
